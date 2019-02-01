ff87ed77a7d50911d1fec71adf46b57bee64140f
youn1223@daum.net <youn1223@daum.net>
2019-02-01 15:02:41 +0900

    album_2019_02_01

--- 15_MyBatis_Album/src/main/java/album/controller/AlbumDeleteController.java
+++ 15_MyBatis_Album/src/main/java/album/controller/AlbumDeleteController.java
@@ -15,20 +15,6 @@ public class AlbumDeleteController {
 	private static final String gotoPage = "redirect:/list.ab";
 	private static final String command = "/delete.ab";
 	
-	@Autowired
-	private AlbumDao albumDao;
 	
-	@RequestMapping(value=command, method=RequestMethod.GET)
-	public String doActionGet( 
-			@RequestParam(value="num",required=true) int num,
-			@RequestParam(value="pageNumber",required=true) int pageNumber){
-		
-		System.out.println("AlbumDeleteController Get ¹æ½Ä µé¾î¿È");
-		int cnt=-1;
-		cnt = albumDao.deleteAlbum(num);
-		
-		return gotoPage+"?pageNumber="+pageNumber; 
-		//redirect:/list.ab?pageNumber="+pageNumber
-	}
 	
 }
